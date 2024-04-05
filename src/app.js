efc2a33e3e6ad64cd258a4c5a10edbb26c95a2fe
Patrick Gross <bears_lair@protonmail.com>
2024-04-05 16:31:00 -0700

    added post request data

--- src/app.js
+++ src/app.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = 3000;
 
+const customers = [
+  {
+    name: "Barry",
+    industry: "car mechanic",
+  },
+  {
+    name: "Kim",
+    industry: "music",
+  },
+  {
+    name: "Amanda",
+    industry: "healthcare",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("hello world!");
+  res.send("Welcome!");
+});
+
+app.get("/api/customers", (req, res) => {
+  res.send({ customers: customers });
+});
+
+app.post("/api/customers", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 app.post("/", (req, res) => {
-  res.send("This is a post request");
+  res.send("This is a post request!");
 });
 
 app.listen(PORT, () => {
